385bae6faa28030f23e8ec8fe5ec4bfdb2c03256
Wes Jonas <praebop@gmail.com>
2015-09-28 23:14:41 -0500

    Go fmt

--- rewrite.go
+++ rewrite.go
@@ -1,12 +1,12 @@
 package rewriter
 
 import (
-"net/http"
-"strings"
+	"net/http"
+	"strings"
 )
 
 func Rewrite(h http.Handler) http.Handler {
-	fn := func (w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
 		if r.RequestURI != "/" {
 			if r.RequestURI[len(r.RequestURI)-1:] == "/" {
 				s := strings.TrimRight(r.URL.Path, "/")

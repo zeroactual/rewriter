c37159752320d4239cca99f434b2b7a4d65bdfe3
Wes Jonas <praebop@gmail.com>
2015-09-28 22:38:46 -0500

    Added rewriter

---
+++ rewrite.go
@@ -0,0 +1,20 @@
+package rewriter
+
+import (
+"net/http"
+"strings"
+)
+
+func Trailing(h http.Handler) http.Handler {
+	fn := func (w http.ResponseWriter, r *http.Request) {
+		if r.RequestURI != "/" {
+			if r.RequestURI[len(r.RequestURI)-1:] == "/" {
+				s := strings.TrimRight(r.URL.Path, "/")
+				http.Redirect(w, r, s, http.StatusFound)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
